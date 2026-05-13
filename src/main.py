f41333b80c0f3cd9f391a9cc44f24f71c397ef5c
skwiera_magic <mckskwiera@gmail.com>
2026-05-13 17:44:22 +0100

    project started

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from textnode import TextNode, TextType
+
+print("hello world")
+
+def main():
+    textnode = TextNode(
+        text = "Is this working?",
+        text_type = TextType.BOLD,
+        url = "www.google.com",
+    )
+
+    print(textnode)
+
+if __name__ == "__main__":
+    main()
