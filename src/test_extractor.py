f879a723feeff292499cce90835734d84e4e7e98
skwiera_magic <mckskwiera@gmail.com>
2026-05-15 14:19:54 +0100

    Links extractor and tests implemented

---
+++ src/test_extractor.py
@@ -0,0 +1,28 @@
+import unittest
+
+from extractor import extract_markdown_images, extract_markdown_links
+
+class TestExtractor(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_wrong_func_link(self):
+        matches = extract_markdown_images(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
+
+    def test_wrong_func_image(self):
+        matches = extract_markdown_links(
+            "This is text with a ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
